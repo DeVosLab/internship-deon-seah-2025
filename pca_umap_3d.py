b2ddf9b306c828e693c9dd21e1fe3be0e6bd91f6
deondoendeno <deonnseahh@gmail.com>
2025-05-13 14:45:40 +0200

    restructured main() and added option to create a clustermap

--- pca_umap_3d.py
+++ pca_umap_3d.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import colorcet
 from matplotlib.colors import Normalize
 import umap
 import hdbscan
@@ -24,6 +26,7 @@ o Create a scatter plot of PCAs where points are coloured as function of:
 o Perform UMAP for better visualisation
 o Provide option to perform HDBScan
 o Add output path to save plots and labels with coords to folder.
+o Provide option to visualise on clustermap
 """
 
 # Find a sample with clear positive and negative cells
@@ -101,11 +104,11 @@ def perform_hdbscan(input_path, sample, method, clustering=False):
 def normalise_cmaps(input_path, sample, coords_type):
     data = convert_coords(input_path, sample, coords_type)
     norm_mi0 = Normalize(
-        vmin=data['intensities_0'].quantile(0.01),
-        vmax=data['intensities_0'].quantile(0.99))
+        vmin=data['intensity_0'].quantile(0.01),
+        vmax=data['intensity_0'].quantile(0.99))
     norm_mi1 = Normalize(
-        vmin=data['intensities_1'].quantile(0.1),
-        vmax=data['intensities_1'].quantile(0.9))
+        vmin=data['intensity_1'].quantile(0.1),
+        vmax=data['intensity_1'].quantile(0.9))
     norm_theta = None
     norm_phi = None
 
@@ -135,6 +138,7 @@ def main(**kwargs):
     method = kwargs['method']
     coords_type = kwargs['coords_type']
     clustering = kwargs['clustering']
+    clustermap = kwargs['clustermap']
 
     # defining params
     if method != 'cartesian':
@@ -146,99 +150,60 @@ def main(**kwargs):
     plots_dir = Path(output_path).joinpath('plots')
     plots_dir.mkdir(exist_ok=True, parents=True) 
 
+    # defining more params for polar
     if coords_type == 'polar':
-        # defining more params for polar
-        colour_fields = ['intensities_0', 'radial_distance', 'theta_angle', 'phi_angle',
-                         'intensities_1', 'radial_distance', 'theta_angle', 'phi_angle']
+        colour_fields = ['intensity_0', 'radial_distance', 'theta_angle', 'phi_angle',
+                         'intensity_1', 'radial_distance', 'theta_angle', 'phi_angle']
         norm_mi0, norm_mi1, norm_theta, norm_phi = normalise_cmaps(input_path, sample, coords_type)
         norms = [norm_mi0, None, norm_theta, norm_phi,
                  norm_mi1, None, norm_theta, norm_phi]
-        
-        for channel, features in enumerate(components):
-            num_plots = 4
-            fig = plt.figure(figsize=(20, 5))
-            for i in range(num_plots):
-                ax = fig.add_subplot(1, num_plots, i+1,
-                                     projection='3d')
-                scatter = ax.scatter(
-                    features[:, 0],
-                    features[:, 1],
-                    features[:, 2],
-                    c=data[colour_fields[i]] if channel == 0 else data[colour_fields[i+4]],
-                    cmap='inferno' if i == 0 or i == 1 else 'RdBu',
-                    norm=norms[i] if channel == 0 else norms[i+4],
-                    s=3)
-                fig.colorbar(scatter, ax=ax, label=colour_fields[i])
-                ax.set_title(f'{method} of Channel {channel} - Coloured by {colour_fields[i] if channel == 0 else colour_fields[i+4]}')
-            fig_path = f'{plots_dir}\\{time_stamp}_{sample}_channel_{channel}_{coords_type}_{method}_plot.png'
-            fig.savefig(fig_path, dpi=300, bbox_inches='tight')
-            print(f'Saved plotted features for: {sample}, Channel {channel}')
-            plt.tight_layout()
     
+    # defining more params for cylindrical
     elif coords_type == 'cylindrical':
-        # defining more params for cylindrical
-        colour_fields = ['intensities_0', 'radial_distance', 'theta_angle', 'z',
-                         'intensities_1', 'radial_distance', 'theta_angle', 'z']
+        colour_fields = ['intensity_0', 'radial_distance', 'theta_angle', 'z',
+                         'intensity_1', 'radial_distance', 'theta_angle', 'z']
         norm_mi0, norm_mi1, norm_theta, norm_phi = normalise_cmaps(input_path, sample, coords_type)
         norms = [norm_mi0, None, norm_theta, None,
                  norm_mi1, None, norm_theta, None]
-        
-        for channel, features in enumerate(components):
-            num_plots = 4
-            fig = plt.figure(figsize=(20, 5))
-            for i in range(num_plots):
-                ax = fig.add_subplot(1, num_plots, i+1,
-                                     projection='3d')
-                scatter = ax.scatter(
-                    features[:, 0],
-                    features[:, 1],
-                    features[:, 2],
-                    c=data[colour_fields[i]] if channel == 0 else data[colour_fields[i+4]],
-                    cmap='inferno' if i == 0 or i == 1 else 'RdBu',
-                    norm=norms[i] if channel == 0 else norms[i+4],
-                    s=3)
-                fig.colorbar(scatter, ax=ax, label=colour_fields[i])
-                ax.set_title(f'{method} of Channel {channel} - Coloured by {colour_fields[i] if channel == 0 else colour_fields[i+4]}')
-            fig_path = f'{plots_dir}\\{time_stamp}_{sample}_channel_{channel}_{coords_type}_{method}_plot.png'
-            fig.savefig(fig_path, dpi=300, bbox_inches='tight')
-            print(f'Saved plotted features for: {sample}, Channel {channel}')
-            plt.tight_layout()
 
-    else: # cartesian
-        # defining more params for cartesian
-        colour_fields = ['intensities_0', 'x', 'y', 'z',
-                         'intensities_1', 'x', 'y', 'z']
+    # defining more params for cartesian
+    else: # cartesian coords
+        colour_fields = ['intensity_0', 'x', 'y', 'z',
+                         'intensity_1', 'x', 'y', 'z']
         norm_mi0, norm_mi1, norm_theta, norm_phi = normalise_cmaps(input_path, sample, coords_type)
         norms = [norm_mi0, None, None, None,
                  norm_mi1, None, None, None]
         
-        for channel, features in enumerate(components):
-            num_plots = 4
-            fig = plt.figure(figsize=(20, 5))
-            for i in range(num_plots):
-                ax = fig.add_subplot(1, num_plots, i+1,
-                                     projection='3d')
-                scatter = ax.scatter(
-                    features[:, 0],
-                    features[:, 1],
-                    features[:, 2],
-                    c=data[colour_fields[i]] if channel == 0 else data[colour_fields[i+4]],
-                    cmap='inferno' if i == 0 or i == 1 else 'RdBu',
-                    norm=norms[i] if channel == 0 else norms[i+4],
-                    s=3)
-                fig.colorbar(scatter, ax=ax, label=colour_fields[i])
-                ax.set_title(f'{method} of Channel {channel} - Coloured by {colour_fields[i] if channel == 0 else colour_fields[i+4]}')
-            fig_path = f'{plots_dir}\\{time_stamp}_{sample}_channel_{channel}_{coords_type}_{method}_plot.png'
-            fig.savefig(fig_path, dpi=300, bbox_inches='tight')
-            print(f'Saved plotted features for: {sample}, Channel {channel}')
-            plt.tight_layout()
+    # for loops to plot features for four variables and for both channels
+    for channel, features in enumerate(components):
+        num_plots = 4
+        fig = plt.figure(figsize=(20, 5))
+        for i in range(num_plots):
+            ax = fig.add_subplot(1, num_plots, i+1,
+                                    projection='3d')
+            scatter = ax.scatter(
+                features[:, 0],
+                features[:, 1],
+                features[:, 2],
+                c=data[colour_fields[i]] if channel == 0 else data[colour_fields[i+4]],
+                cmap='inferno' if i == 0 or i == 1 else 'RdBu',
+                norm=norms[i] if channel == 0 else norms[i+4],
+                s=3)
+            fig.colorbar(scatter, ax=ax, label=colour_fields[i])
+            ax.set_title(f'{method} of Channel {channel} - Coloured by {colour_fields[i] if channel == 0 else colour_fields[i+4]}')
+        fig_path = f'{plots_dir}\\{time_stamp}_{sample}_channel_{channel}_{coords_type}_{method}_plot.png'
+        fig.savefig(fig_path, dpi=300, bbox_inches='tight')
+        print(f'Saved plotted features for: {sample}, Channel {channel}')
+        plt.tight_layout()
 
+    # perform HDBSCAN clustering
     if clustering:
         print(f'Performing HDBSCAN clustering...')
         labels = perform_hdbscan(input_path, sample, method, clustering) # labels0, labels1
         num_plots = 2
         fig = plt.figure(figsize=(10, 5))
-
+        
+        # plot features for each channel, coloured by HDBSCAN labels
         for channel, components_channel in enumerate(components):
             ax = fig.add_subplot(1, num_plots, channel+1,
                                  projection='3d')
@@ -255,7 +220,7 @@ def main(**kwargs):
             fig.savefig(fig_path, dpi=300, bbox_inches='tight')
         plt.tight_layout()
 
-        # Saving labels and coordinates to .csv file
+        # save labels and coordinates to .csv file
         print('Saving labels and coordinates...')
 
         labels0, labels1 = labels
@@ -272,7 +237,30 @@ def main(**kwargs):
         df1 = pd.DataFrame(data.iloc[:, 4:7], columns=['z', 'y', 'x'])
         df1['label'] = labels1
         df1.to_csv(f'{labels_dir}\\{time_stamp}_{sample}_labels_with_coords_channel_1.csv', index=False)
+
+        # visualise features against channel labels in a clustermap
+        if clustermap:
+            cluster_channel = kwargs['cluster_channel']
+            
+            if coords_type == 'polar':
+                cluster_data = data[['radial_distance', 'theta_angle', 'phi_angle']]
+
+            elif coords_type == 'cylindrical':
+                cluster_data = data[['radial_distance', 'theta_angle', 'z']]
+
+            else:
+                cluster_data = data[['z', 'y', 'x']]
+
+            index = cluster_data.index
+
+            row_col = pd.Series(labels[cluster_channel].labels_, index=index).map(dict(zip(np.unique(labels[cluster_channel].labels_),
+                                                        sns.color_palette('hls', len(np.unique(labels[cluster_channel].labels_))))))
+            
+            sns.clustermap(cluster_data, standard_scale=1, row_colors=row_col,
+                           row_cluster=False, col_cluster=False)
         
+        else:
+            pass
     else:
         pass
 
@@ -293,6 +281,10 @@ def parse_args():
                         help='Type of coordinates to colour points by')
     parser.add_argument('--clustering', action='store_true',
                         help='Cluster with HDBScan')
+    parser.add_argument('--clustermap', action='store_true',
+                        help='Plots a hierarchically-clustered heatmap')
+    parser.add_argument('--cluster_channel', type=int, default=0,
+                        help='Choice of channel number to cluster points on')
     args = parser.parse_args()
 
     if not args.method.isupper():
